1e2af4f2e310b4b126f71ecd447eaa97d0991687
Ivan Grishkov <grishkov.i.a@gmail.com>
2015-12-11 18:33:20 +0600

    lesson 1p1 equation

---
+++ lesson1/part1/equation.rb
@@ -0,0 +1,18 @@
+puts "Введите коэфициенты"
+print "a = "
+a = gets.chomp.to_f
+print "b = "
+b = gets.chomp.to_f
+print "c = "
+c = gets.chomp.to_f
+
+d = b**2 - 4*a*c
+
+
+if d = 0 
+	puts "D = 0, корень уравнения Х = #{-b/2/a}"
+elsif d > 0
+	puts "D = #{d}, Х1 = #{(-b+Math.sqrt(d))/2/a}, Х2 = #{(-b-Math.sqrt(d))/2/a}"
+else
+	puts "D = #{d}, корней нет"
+end
