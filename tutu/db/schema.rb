969dfca9a70cb57964e5d009e2b1f72e83b29979
Ivan Grishkov <grishkov.i.a@gmail.com>
2016-01-28 15:33:17 +0600

    trains

--- tutu/db/schema.rb
+++ tutu/db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128090927) do
+ActiveRecord::Schema.define(version: 20160128092903) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trains", force: :cascade do |t|
+    t.string   "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
