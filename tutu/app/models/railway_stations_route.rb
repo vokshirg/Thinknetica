cd229eeac6e25350f0ad75864e31437be131e3ad
Ivan Grishkov <grishkov.i.a@gmail.com>
2016-02-01 11:53:23 +0600

    validates corrections

--- tutu/app/models/railway_stations_route.rb
+++ tutu/app/models/railway_stations_route.rb
@@ -1,10 +1,10 @@
 # Join-table between RailwayStations and Trains
 
 class RailwayStationsRoute < ActiveRecord::Base
-  # validates :railway_stations_routes, uniqueness: {
-    # scope: [:railway_station, :route],
-    # message: "В маршруте не могут повторяться станции"
-  # }
+  validates :route, uniqueness: {
+    scope: :railway_station,
+    message: "В маршруте не могут повторяться станции"
+  }
 
   belongs_to :route
   belongs_to :railway_station
