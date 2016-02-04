66dd227d7c04f842772de4754a945038a2b2d6ed
Ivan Grishkov <grishkov.i.a@gmail.com>
2016-02-04 22:07:16 +0600

    add id to validation

--- tutu/app/models/ticket.rb
+++ tutu/app/models/ticket.rb
@@ -8,6 +8,6 @@ class Ticket < ActiveRecord::Base
   belongs_to :start_station, class_name: "RailwayStation", foreign_key: :start_station_id
 
   # validates :number, presence: true
-  validates :seat, :end_station, :start_station, presence: true
+  validates :seat, :end_station_id, :start_station_id, presence: true
 
 end
