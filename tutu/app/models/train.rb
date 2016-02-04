50a8401e6d186f7fd80006e3efcfa4458adcd317
Ivan Grishkov <grishkov.i.a@gmail.com>
2016-02-04 21:46:34 +0600

    top&bottom places divide

--- tutu/app/models/train.rb
+++ tutu/app/models/train.rb
@@ -12,22 +12,16 @@ class Train < ActiveRecord::Base
     wagons.where(wagon_type: type)
   end
 
-  # def all_top_places(type)
-  #   @all_top_places = 0
-  #   wagons_by_type(type).each {|w| @all_top_places += w.top_places}
-  #   return @all_top_places
-  # end
+  def all_top_places(type)
+    @all_top_places = 0
+    wagons_by_type(type).each {|w| @all_top_places += w.top_places}
+    return @all_top_places
+  end
 
-  # def all_bottom_places(type)
-  #   @all_bottom_places = 0
-  #   wagons_by_type(type).each {|w| @all_bottom_places += w.bottom_places}
-  #   return @all_bottom_places
-  # end
-
-  def all_places(type)
-    @all_places = [0,0]
-    wagons_by_type(type).each {|w| @all_places[0] += w.top_places; @all_places[1] += w.bottom_places}
-    return @all_places
+  def all_bottom_places(type)
+    @all_bottom_places = 0
+    wagons_by_type(type).each {|w| @all_bottom_places += w.bottom_places}
+    return @all_bottom_places
   end
 
 end
