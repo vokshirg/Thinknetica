20cae350b2ca254ff258e8b5f2d5f69c55c1ba90
Ivan Grishkov <grishkov.i.a@gmail.com>
2016-02-19 15:16:19 +0600

    all without search

--- tutu/config/routes.rb
+++ tutu/config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
 
   end
 
+  resource :search, only: [:new, :show, :edit]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
