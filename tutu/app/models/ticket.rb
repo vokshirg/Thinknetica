e896321a8efc43e983908547c6cbb92890ab931f
Ivan Grishkov <grishkov.i.a@gmail.com>
2016-03-02 12:30:09 +0600

    del ticket mail

--- tutu/app/models/ticket.rb
+++ tutu/app/models/ticket.rb
@@ -7,7 +7,8 @@ class Ticket < ActiveRecord::Base
   belongs_to :end_station, class_name: "RailwayStation", foreign_key: :end_station_id
   belongs_to :start_station, class_name: "RailwayStation", foreign_key: :start_station_id
 
-  after_create :send_notification
+  after_create :buy_ticket_mail
+  after_destroy :del_ticket_mail
 
   # validates :number, presence: true
   # validates :seat, :end_station_id, :start_station_id, presence: true
@@ -26,8 +27,12 @@ class Ticket < ActiveRecord::Base
 
   private
 
-  def send_notification
+  def buy_ticket_mail
     TicketsMailer.buy_ticket(self.passanger, self).deliver_now
+  end
+
+  def del_ticket_mail
+    TicketsMailer.del_ticket(self).deliver_now
   end
 
 end
