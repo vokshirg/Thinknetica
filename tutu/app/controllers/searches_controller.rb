e1779936e1cedf94b7de50a9ea995a03ec840779
Ivan Grishkov <grishkov.i.a@gmail.com>
2016-03-02 12:20:00 +0600

    error handling in search

--- tutu/app/controllers/searches_controller.rb
+++ tutu/app/controllers/searches_controller.rb
@@ -4,14 +4,28 @@ class SearchesController < ApplicationController
   end
 
   def find
-    @start_station = RailwayStation.find(params[:start_station])
-    @end_station = RailwayStation.find(params[:end_station])
-    trains_start_st = Train.joins(route: :railway_stations).where("railway_station_id = ?" , @start_station)
-    trains_end_st =   Train.joins(route: :railway_stations).where("railway_station_id = ? ", @end_station)
+    unless params[:start_station].blank? && params[:end_station].blank?
+      @start_station = RailwayStation.find(params[:start_station])
+      @end_station = RailwayStation.find(params[:end_station])
 
-    @results = trains_start_st & trains_end_st
+      if @start_station == @end_station
+        flash.now[:error] = 'Станции не могут быть равны'
+        render :show
+      else
+        trains_start_st = Train.joins(route: :railway_stations).where("railway_station_id = ?" , @start_station)
+        trains_end_st =   Train.joins(route: :railway_stations).where("railway_station_id = ? ", @end_station)
 
-    render :show, start_station: params[:start_station], end_station: params[:end_station]
+        @results = trains_start_st & trains_end_st
+
+        render :show, start_station: params[:start_station], end_station: params[:end_station]
+      end
+    else
+
+      flash.now[:error] = 'Укажите начальную и конечную станцию'
+      render :show
+    end
+
+
   end
 
 end
